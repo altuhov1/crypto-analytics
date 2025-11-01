aaf6d8d9fb61a3702e97130bd9ce3947e92eaf41
Dmitriy Altuhov <D.Altuhov1@gmail.com>
2025-11-01 13:58:37 +0300

    some fix

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log/slog"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"os/signal"
 	"syscall"
@@ -205,7 +206,13 @@ func (a *App) setupRoutes(handler *handlers.Handler) http.Handler {
 	for path, handlerFunc := range webRoutes {
 		mux.HandleFunc(path, handlerFunc)
 	}
-
+	if a.cfg.LaunchLoc != "prod" {
+		mux.HandleFunc("/debug/pprof/", pprof.Index)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	}
 	// Root route
 	mux.HandleFunc("/", a.rootHandler)
 
